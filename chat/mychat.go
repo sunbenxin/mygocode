76e6300c0c21352058025276bcbdce9ec4972b77
sunbx <sunbenxin@p1.com>
2017-02-21 21:43:38 +0800

    init

---
+++ chat/mychat.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+// client represents a single chatting user.
+type client struct {
+	// socket is the web socket for this chatting user.
+	socket *websocket.Conn
+	// send is a channel on which message are sent.
+	send chan []byte
+	// room is the room this client is chatting in.
+	room *room
+}
+
+func (c *client) read() {
+	defer c.socket.Close()
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.room.forward <- msg
+	}
+}
+
+func (c *client) write() {
+	defer c.socket.Close()
+	for msg := range c.send {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
+		}
+	}
+}
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
+			t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// get the room going
+	go r.run()
+	// start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
